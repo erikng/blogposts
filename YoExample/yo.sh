12bef9dd9566ca22b585723edaf53bcced370058
erikng <erikng@users.noreply.github.com>
2015-03-26 20:17:33 -0500

    first commit

---
+++ YoExample/yo.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+/Applications/Utilities/Yo.app/Contents/MacOS/yo\
+	-t "Yo is awesome" \
+	-s "To view the document" \
+	-n "Please click on Open PDF" \
+	-b "Open PDF" \
+	-a "/Library/Documentation/Yo.pdf" \
+	-p \
+	-z "/System/Library/PrivateFrameworks/ToneLibrary.framework/Versions/A/Resources/AlertTones/Modern/sms_alert_note"
